19d23787fca6f0eb6d47770961395459e7fcfcd4
Natalia Gatti <natalia@xcapit.com>
2024-10-03 06:05:13 -0300

    Test required data at checkout and alerts

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,7 +1,8 @@
 import pytest
+import time
+from selenium.common.exceptions import NoAlertPresentException
 from pages.cart_page import CartPage
 from pages.product_page import ProductPage
-import time
 
 from pages.purchase_page import PurchasePage
 
@@ -102,4 +103,80 @@ def test_remove_from_cart(setup_driver):
   cart_items = cart_page.get_cart_products()
   assert len(cart_items) == 0
 
+def test_required_data(setup_driver):
+  # Requiere nombre y Tarjeta de crédito (sólo número)
+   # Step 1: Navigate to the products page
+  product_page = ProductPage(setup_driver)
+  
+  # Step 2: Select a product and go to its page
+  product_page.select_product() 
+
+  # Step 3: Add the selected product to the cart
+  product_page.add_product_to_cart()
+  
+  # Step 4: Close the confirmation modal
+  time.sleep(2)
+  alert = setup_driver.switch_to.alert
+  alert.accept()
+  
+  # Step 5: Navigate to the cart and proceed to checkout
+  product_page.navigate_to_products_cart()
+  cart_page = CartPage(setup_driver)
+  cart_page.place_order()
+
+  # Step 6: Fill out the purchase form
+  purchase_page = PurchasePage(setup_driver)
+  purchase_page.fill_purchase_form(
+      name='',
+      country='Argentina',
+      city='Buenos Aires',
+      card='',
+      month='12',
+      year='2024'
+  )
+  
+  # Step 7: Verify the error message
+  time.sleep(2)
+  alert = setup_driver.switch_to.alert
+  assert alert.text == 'Please fill out Name and Creditcard.', 'The alert not appear'
+  
+def test_no_blank_required_data(setup_driver):
+  # Step 1: Navigate to the products page
+  product_page = ProductPage(setup_driver)
+  
+  # Step 2: Select a product and go to its page
+  product_page.select_product() 
+
+  # Step 3: Add the selected product to the cart
+  product_page.add_product_to_cart()
+  
+  # Step 4: Close the confirmation modal
+  time.sleep(2)
+  alert = setup_driver.switch_to.alert
+  alert.accept()
+  
+  # Step 5: Navigate to the cart and proceed to checkout
+  product_page.navigate_to_products_cart()
+  cart_page = CartPage(setup_driver)
+  cart_page.place_order()
+
+  # Step 6: Fill out the purchase form
+  purchase_page = PurchasePage(setup_driver)
+  purchase_page.fill_purchase_form(
+      name='  ',
+      country='Argentina',
+      city='Buenos Aires',
+      card='  ',
+      month='12',
+      year='2024'
+  )
+  
+  # Step 7: Verify the error message
+  time.sleep(2)
+  try:
+      alert = setup_driver.switch_to.alert
+      assert alert.text == 'Please fill out Name and Creditcard.'
+      alert.accept()
+  except NoAlertPresentException:
+      assert False, 'The alert did not appear'
   
